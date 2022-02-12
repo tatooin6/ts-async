8cdbbfcf365ef8f0ed6207fdcc7de60e178b8f61
Antonio Pantoja <tato_fos@hotmail.com>
2022-02-11 23:06:26 -0400

    Function Overloading

--- callbacks.ts
+++ callbacks.ts
@@ -10,15 +10,32 @@ export interface PokemonList {
   }[];
 }
 
-export const getPokemonList = (
+// Signatures
+function getPokemonList (
   cb: (err: Error | undefined, pokemonList: PokemonList | undefined) => void
-): void => {
-axios.get("https://pokeapi.co/api/v2/pokemon")
-.then((resp)=>{return resp.data as PokemonList})
-.then((data:PokemonList)=>cb(undefined, data))
-.catch((err) => cb(err, undefined));
+): void; 
+function getPokemonList (): Promise<PokemonList> | void; 
+
+function getPokemonList (
+  cb?: (err: Error | undefined, pokemonList: PokemonList | undefined) => void
+): Promise<PokemonList> | void {
+  if(cb) {
+    axios.get("https://pokeapi.co/api/v2/pokemon")
+      .then((resp)=>{return resp.data as PokemonList})
+      .then((data:PokemonList)=>cb(undefined, data))
+      .catch((err) => cb(err, undefined));
+    return undefined;
+  } else {
+    return axios.get("https://pokeapi.co/api/v2/pokemon")
+      .then((resp)=>{return resp.data as PokemonList})
+  }
 }
 
 getPokemonList((_err,data) => {
-  console.log(data)
-})
+  console.log(data?.results.length)
+});
+
+(async function(){
+  const list = await getPokemonList()
+  console.log(list?.results.length);
+})()
